4618c427d793769332eeb67d7250103b9b80ed60
Tiago Santos <tiagossilva_95@hotmail.com>
2018-02-04 20:10:21 +0000

    Week Weather Cell's components.

---
+++ Cloudy/View Controllers/Root View Controller/Cells/Week Weather Cell/WeekWeatherCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeekWeatherCell.swift
+//  Cloudy
+//
+//  Created by Tiago Santos on 04/02/18.
+//  Copyright © 2018 Tiago Santos. All rights reserved.
+//
+
+import UIKit
+
+class WeekWeatherCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
