d1b8afc24c0ad9cd2673faa0cf7f1568cb7b0ca1
Tiago Santos <tiagossilva_95@hotmail.com>
2018-02-08 23:43:17 +0000

    Refractored the Locations View Controller to use the View Model.

--- Cloudy/View Controllers/Locations View Controller/Add Location View Controller/AddLocationViewController.swift
+++ Cloudy/View Controllers/Locations View Controller/Add Location View Controller/AddLocationViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import CoreLocation
 
 protocol AddLocationViewControllerDelegate {
     func controller(_ controller: AddLocationViewController, didAddLocation location: Location)
@@ -17,29 +16,46 @@ class AddLocationViewController: UIViewController {
 
     // MARK: - View Components
     
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var searchBar: UISearchBar!
-    
-    // MARK: -
-    
-    private var locations: [Location] = []
-    
-    // MARK: -
-    
-    private lazy var geocoder = CLGeocoder()
     
     // MARK: - Delegates
     
     var delegate: AddLocationViewControllerDelegate?
     
+    // MARK: - View Model
+    
+    var viewModel: AddLocationViewModel!
+    
     // MARK: - View Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViewModel()
         setupView()
     }
     
+    // MARK: - View Model Functions
+    
+    func setupViewModel() {
+        viewModel = AddLocationViewModel()
+        
+        viewModel.locationsDidChange = { [unowned self] (locations) in
+            self.tableView.reloadData()
+        }
+        
+        viewModel.queryingDidChange = { [unowned self] (querying) in
+            if querying {
+                self.activityIndicatorView.startAnimating()
+            } else {
+                self.activityIndicatorView.stopAnimating()
+            }
+        }
+    }
+    
     // MARK: - Setups
+    
     func setupView() {
         title = "Add Location"
         registerLocationCell()
@@ -68,46 +84,11 @@ class AddLocationViewController: UIViewController {
     func setupNavigationButtons() {
         
     }
-    
-    // MARK: - Helper methods
-    private func geocode(addressString: String?) {
-        guard let addressString = addressString else {
-            locations = []
-            tableView.reloadData()
-            return
-        }
-        
-        // Geocode City
-        geocoder.geocodeAddressString(addressString) { [weak self] (placemarks, error) in
-            DispatchQueue.main.async {
-                self?.processResponse(withPlacemarks: placemarks, error: error)
-            }
-        }
-    }
-    
-    // MARK: -
-    
-    private func processResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
-        if let error = error {
-            print("Unable to Forward Geocode Address (\(error))")
-            
-        } else if let matches = placemarks {
-            // Update Locations
-            locations = matches.flatMap({ (match) -> Location? in
-                guard let name = match.name else { return nil }
-                guard let location = match.location else { return nil }
-                return Location(name: name, latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-            })
-            
-            // Update Table View
-            tableView.reloadData()
-        }
-    }
 }
 
 extension AddLocationViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return locations.count
+        return viewModel.numberOfLocations
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -115,8 +96,10 @@ extension AddLocationViewController: UITableViewDataSource {
             fatalError("Unexpected Table View Cell")
         }
         
-        let location = locations[indexPath.row]
-        let viewModel = LocationsViewModel(location: location.location, locationAsString: location.name)
+        guard let viewModel = viewModel.viewModelForLocation(at: indexPath.row) else {
+            return cell
+        }
+        
         cell.configure(withViewModel: viewModel)
         return cell
     }
@@ -124,7 +107,7 @@ extension AddLocationViewController: UITableViewDataSource {
 
 extension AddLocationViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let location = locations[indexPath.row]
+        guard let location = viewModel.location(at: indexPath.row) else { return }
         
         // Notify Delegate
         delegate?.controller(self, didAddLocation: location)
@@ -140,16 +123,14 @@ extension AddLocationViewController: UISearchBarDelegate {
         searchBar.resignFirstResponder()
         
         // Forward Geocode Address String
-        geocode(addressString: searchBar.text)
+        viewModel.query = searchBar.text ?? ""
     }
     
     func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
         // Hide Keyboard
         searchBar.resignFirstResponder()
         
-        // Clear Locations
-        locations = []
-        
-        tableView.reloadData()
+        // Forward Geocode Address String
+        viewModel.query = searchBar.text ?? ""
     }
 }
